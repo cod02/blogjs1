270d11239d180731b446644dfa50df964d381c84
Manish Kumar <93198083+cod02@users.noreply.github.com>
2021-10-29 16:22:48 +0530

    Update lazyloadyt.js

--- lazyloadyt.js
+++ lazyloadyt.js
@@ -1,3 +1,26 @@
+function darkMode() {
+    localStorage.setItem("mode", "darkmode" === localStorage.getItem("mode") ? "light" : "darkmode"), "darkmode" === localStorage.getItem("mode") ? document.querySelector("#mainContent").classList.add("darkMode") : document.querySelector("#mainContent").classList.remove("darkMode")
+};
+
+function gridMode() {
+    localStorage.setItem("list", "listmode" === localStorage.getItem("list") ? "grid" : "listmode"), "listmode" === localStorage.getItem("list") ? document.querySelector("#mainContent").classList.add("gridMode") : document.querySelector("#mainContent").classList.remove("gridMode")
+};
+
+function resizeHeaderOnScroll() {
+    const distanceY = window.pageYOffset || document.documentElement.scrollTop,
+        shrinkOn = 20,
+        headerEl = document.getElementById( & #39;header&# 39;);
+    if (distanceY > shrinkOn) {
+        headerEl.classList.add("show");
+    } else {
+        headerEl.classList.remove("show");
+    }
+}
+window.addEventListener( & #39;scroll&# 39;, resizeHeaderOnScroll);
+
+function copyFunction() {
+    document.getElementById("getlink").select(), document.execCommand("copy"), document.getElementById("shareNotif").innerHTML = "<span>Link copied!</span>"
+};
 (function () {
     var youtube = document.querySelectorAll(".lazyYoutube");
     for (var i = 0; i < youtube.length; i++) {
